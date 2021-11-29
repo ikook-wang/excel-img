a33499cece6f682d78987b1cc456c474c02d5933
ikook <ikook.dev@gmail.com>
2021-11-29 16:20:14 +0800

    完成

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use calamine::{open_workbook, Error, Xlsx, Reader, RangeDeserializerBuilder};
+use calamine::{open_workbook, Xlsx, Reader};
 use anyhow::{anyhow, Result};
 use clap::Parser;
-use reqwest::{header, Client, Response, Url};
-use std::{collections::HashMap, str::FromStr};
+use tokio::{fs::OpenOptions, io::AsyncWriteExt};
+use std::path::Path;
+use std::ffi::OsStr;
+use std::str::FromStr;
 
 // 以下部分用于处理 CLI
 
@@ -20,25 +22,125 @@ struct Opts {
 
 #[derive(Parser, Debug)]
 enum SubCommand {
-    path(Path)
+    Save(Save)
 }
 
 /// feed get with an file path and we will retrieve the response for you
 #[derive(Parser, Debug)]
-struct Path {
-    /// 需要解析的文件路径
-    #[clap(parse(try_from_str = parse_path))]
+struct Save {
+    /// excel 文件路径
     path: String,
+
+    /// 文件解析列数，格式为："x,y"，x 为文件名列；y 为图片url列
+    #[clap(parse(try_from_str = parse_kv_pair))]
+    body: KvPair,
+
+    /// 图片保存路径
+    save_path: String,
 }
 
-fn parse_path(s: &str) -> Result<String> {
-    // 这里我们仅仅检查一下 URL 是否合法
-    let _url: Url = s.parse()?;
-
-    Ok(s.into())
+#[derive(Debug)]
+struct KvPair {
+    sort_id: usize,
+    url: usize,
 }
 
+/// 当我们实现 FromStr trait 后，可以用 str.parse() 方法将字符串解析成 KvPair
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
 
-fn main() {
-    println!("Hello, world!");
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // 使用 = 进行 split，这会得到一个迭代器
+        let mut split = s.split(",");
+        let err = || anyhow!(format!("Failed to parse {}", s));
+        Ok(Self {
+            // 从迭代器中取第一个结果作为 key，迭代器返回 Some(T)/None
+            // 我们将其转换成 Ok(T)/Err(E)，然后用 ? 处理错误
+            sort_id: (split.next().ok_or_else(err)?).to_string().parse::<usize>().unwrap(),
+            // 从迭代器中取第二个结果作为 value
+            url: (split.next().ok_or_else(err)?).to_string().parse::<usize>().unwrap(),
+        })
+    }
+}
+
+/// 因为我们为 KvPair 实现 FromStr，这里可以直接 s.parse() 得到 KvPair
+fn parse_kv_pair(s: &str) -> Result<KvPair> {
+    Ok(s.parse()?)
+}
+
+/// 处理 path 子命令
+async fn path(args: &Save) -> Result<()> {
+    let mut excel: Xlsx<_> = open_workbook(&args.path).unwrap();
+
+    if let Some(Ok(r)) = excel.worksheet_range("Sheet1") {
+        let mut i = 0;
+        println!("处理总数为：{:?}", r.get_size().0 - 1);
+        for row in r.rows() {
+            if i == 0 {
+                i = i + 1;
+                continue;
+            } else {
+                println!("正在处理第{}个文件", i);
+                i = i + 1;
+            }
+
+
+            let url = row[args.body.url].to_string();
+
+            let mut filename = args.save_path.to_string();
+            let sort_id = row[args.body.sort_id].to_string();
+
+            let c = ".".to_string();
+            let mut response  = get(&url).await?;
+            let suffix_o = get_extension_from_filename(&url);
+            let suf = suffix_o.unwrap().to_string();
+
+            filename = filename + &sort_id + &c + &suf;
+            save(&filename, &mut response).await?;
+        }
+
+        println!("处理完毕！！！");
+    }
+    Ok(())
+}
+
+fn get_extension_from_filename(filename: &str) -> Option<&str> {
+    Path::new(filename)
+        .extension()
+        .and_then(OsStr::to_str)
+}
+
+async fn save(filename: &str, response: &mut reqwest::Response) -> Result<()> {
+    let mut options = OpenOptions::new();
+    let mut file = options
+        .append(true)
+        .create(true)
+        .read(true)
+        .open(filename)
+        .await?;
+
+    while let Some(chunk) = &response.chunk().await.expect("Failed") {
+        match file.write_all(&chunk).await {
+            Ok(_) => {}
+            Err(e) => return Err(anyhow!("File {} save error: {}", filename, e)),
+        }
+    }
+    Ok(())
+}
+
+async fn get(url: &str) -> Result<reqwest::Response> {
+    reqwest::get(url)
+        .await
+        .map_err(|e| anyhow!("Request url {} error: {}", url, e))
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let opts: Opts = Opts::parse();
+
+    let result = match opts.subcmd {
+        SubCommand::Save(ref args) => path(args).await?,
+    };
+
+    Ok(result)
 }
